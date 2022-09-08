0ba982baab43ac217e19ebecc6b3666e4056b66b
Jonathan <89255112+jagharloggatin@users.noreply.github.com>
2022-09-08 06:24:19 +0200

    how to use formidable upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const formidable = require('formidable');
+const path = require("path");
+const fs = require("fs");
+const cors = require('cors');
+
+const libraryDir = "app-data";
+const applicationDir = path.resolve('./');
+
+const app = express();
+
+//To get past cors policy
+app.use(cors({
+    origin: '*'
+}));
+
+app.listen(3000, () => {
+    console.log('Server listening on http://localhost:3000 ...');
+});
+
+
+//Post request
+app.post('/api/upload', (req, res) => {
+
+    //Creates a formidable object
+    let form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+
+        //Creates a directory with the name of the title that the user chose
+        fs.mkdirSync(path.resolve(`app-data/library/pictures/${fields.title}`));
+
+        //Files old path when uploaded with formidable
+        let oldPath = files.image.filepath;
+
+        // let newPath = path.resolve('appdata/library/pictures' + files.image.originalFilename);
+        //Puts the file in the new path
+        let newPath = 'C:\\Users\\Jonathan\\WebstormProjects\\formidable\\app-data\\library\\pictures\\albumheaderimage\\' + files.image.originalFilename;
+
+        fs.rename(oldPath, newPath, function (err) {
+            if (err) throw err;
+            res.write('File uploaded and moved!');
+            res.end();
+        });
+
+        //Writes image info to albumCache.json to later unpack and use
+        writeJSON('albumCache.json', {err, fields, files});
+        res.json({ fields, files });
+    });
+});
+
+
+//Converts fileinfo to JSON
+function writeJSON(fname, obj) {
+    const dir = path.join(applicationDir, `/${libraryDir}`);
+    fs.writeFileSync(path.resolve(dir, fname), JSON.stringify(obj));
+}
+
---
+++ submit.js
@@ -0,0 +1,36 @@
+const formAddServerDictory = document.getElementById('formAddServerDictory');
+
+const urlPost = 'http://localhost:3000/api/upload';
+const urlJson = './app-data/albumCache.json';
+
+formAddServerDictory.addEventListener('submit', async event => {
+    event.preventDefault();
+
+    //Create the key/value pairs used in the form
+    const formData = new FormData(formAddServerDictory);
+    try {
+        //send the data using post and await the reply
+        const response = await fetch(urlPost, {
+            method: 'post',
+            body: formData
+        });
+        const result = await response.text();
+
+        if (response.ok) {
+
+            const response = await fetch(urlJson);
+            const data = await response.text();
+
+            alert(`Thank you for submitting the information. It has been recieved:\n`+
+                `${data}`);
+
+        }
+        else {
+            alert("Transmission error");
+        }
+        console.log(result);
+    }
+    catch {
+        alert("Transmission error");
+    }
+})
